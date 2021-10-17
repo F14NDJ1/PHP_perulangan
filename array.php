ea0d2770426f555482a84fb5b66d28184de322d5
F14NDJ1E <firmanuciha@gmail.com>
2021-10-17 15:56:45 +0800

    perulangan

---
+++ array.php
@@ -0,0 +1,47 @@
+<?php 
+	$data_artikel = array (
+		(object) array (
+			"judul" => "Card 1",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/7422238/pexels-photo-7422238.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 2",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/7422238/pexels-photo-7422238.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 3",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/7422238/pexels-photo-7422238.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 4",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/7422238/pexels-photo-7422238.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			)
+		);
+
+	$data_carousel = array (
+		(object) array (
+			"judul" => "Card 1",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/6785291/pexels-photo-6785291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 2",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/4727484/pexels-photo-4727484.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 3",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/9726123/pexels-photo-9726123.png?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			),
+		(object) array (
+			"judul" => "Card 4",
+			"text" => "lorem ipsum doler leanga sowao sis toeamo.",
+			"gambar" => "https://images.pexels.com/photos/6947982/pexels-photo-6947982.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			)
+		);	
+ ?>
